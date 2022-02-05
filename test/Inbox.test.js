908562a85e0e5c1de2a69ad1f72cb37bdbf81b19
Daniel Andrade <dcca12@gmail.com>
2022-02-05 12:12:49 -0300

    testing with mocha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,3 +5,33 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 // Invoking a instance of web3 using the contructor
 const web3 = new Web3(ganache.provider());
+
+// Testing with Mocha
+// // Starting with Mocha
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+
+//     drive() {
+//         return 'vrumm'
+//     }
+// }
+
+// // Declare var to tests
+// let car;
+// // 
+// beforeEach(()=> {
+//     car = new Car();
+// })
+
+// // Defining a test
+// describe('Car', () => {
+//     it('can park', () => {
+//         // test case
+//         assert.equal(car.park(), 'stopped')
+//     })
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vrumm')
+//     })
+// });
