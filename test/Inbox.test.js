90684af961d9f3c5aca693ab24119e795ac6e3d5
Daniel Andrade <dcca12@gmail.com>
2022-02-05 11:46:40 -0300

    starting web3 on testing file

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,7 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+// Web3 constructor function
+const Web3 = require('web3');
+// Invoking a instance of web3 using the contructor
+const web3 = new Web3(ganache.provider());
